588cde5cdc56fc57c4b1a75634147f3ff94e291a
Joeri Nicolaes <joerinicolaes@gmail.com>
2017-01-05 12:22:34 +0000

    vectorized toss and grad completed and test ok in ipynb

--- classifiers/linear_svm.py
+++ classifiers/linear_svm.py
@@ -37,13 +37,16 @@ def svm_loss_naive(W, X, y, reg):
 			# 		dW[i,j] += -marg
 			# dW[i,j] *= X[i,j]
 			continue
+		# alternative code 
+		# accumulate loss for the i-th example
+    	# loss_i += max(0, scores[j] - correct_class_score + delta)
 		margin = scores[j] - correct_class_score + 1
 		if margin > 0:
 			diff_count += 1
 			loss += margin
 			dW[:,j] += X[i]	# gradient update for j != y_j
 	# gradient update for j == y_j
-	dW[:, y[i]] += -diff_count * X[i]
+	dW[:,y[i]] += -diff_count * X[i]
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
@@ -89,28 +92,37 @@ def svm_loss_vectorized(W, X, y, reg):
   loss = 0.0
   dW = np.zeros(W.shape) # initialize the gradient as zero
 
+  num_classes = W.shape[1]
+  num_train = X.shape[0]
+  delta = 1.0
+
   #############################################################################
   # TODO:                                                                     #
   # Implement a vectorized version of the structured SVM loss, storing the    #
   # result in loss.                                                           #
   #############################################################################
   
-  # see https://github.com/bruceoutdoors/CS231n/blob/master/assignment1/cs231n/classifiers/linear_svm.py
+  # half-vectorized from http://cs231n.github.io/linear-classify/#svm
+  # delta = 1.0
+  # scores = W.dot(x)
+  # # compute the margins for all classes in one vector operation
+  # margins = np.maximum(0, scores - scores[y] + delta)
+  # # on y-th position scores[y] - scores[y] canceled and gave delta. We want
+  # # to ignore the y-th position and only consider margin on max wrong class
+  # margins[y] = 0
+  # loss_i = np.sum(margins)
+  # return loss_i
 
-  # create y mask to put all y_j values to zero
-  # y = y.reshape(y.shape[0], 1) 
-  mask = np.ones((y.shape[0], W.shape[1]))
-  c = 0
-  for item in y:
-	mask[item,c] = 0
-	c+=1
+  # inspired by tutorial https://github.com/bruceoutdoors/CS231n/blob/master/assignment1/cs231n/classifiers/linear_svm.py
 
-  matmul = X.dot(W)
-  data_loss = 1/X.shape[0] * np.sum((matmul - matmul[y])* mask + 1)  #delta = 1
+  scores = X.dot(W)
+  correct_class_scores = scores[np.arange(num_train),y]
+  margins = np.maximum(0, scores - correct_class_scores[:, np.newaxis] + delta)
+  margins[np.arange(num_train),y] = 0	# on y-th position: set margin to 0 (sum excludes j == y_j)
 
-  regu_loss = 0.5 * reg * np.sum(W * W)
-
-  loss = data_loss + regu_loss
+  loss = np.sum(margins)
+  loss /= num_train	#get mean across samples
+  loss += 0.5 * reg * np.sum(W * W)		#regularization
 
   #############################################################################
   #                             END OF YOUR CODE                              #
@@ -126,7 +138,31 @@ def svm_loss_vectorized(W, X, y, reg):
   # to reuse some of the intermediate values that you used to compute the     #
   # loss.                                                                     #
   #############################################################################
-  dW = 0
+  
+  # # Semi-vectorized version... It's not any faster than the loop version.
+  # num_classes = W.shape[1]
+  # incorrect_counts = np.sum(margins > 0, axis=1)
+  # for k in xrange(num_classes):
+	 #  # use the indices of the margin array as a mask.
+	 #  wj = np.sum(X[margins[:, k] > 0], axis=0)
+	 #  wy = np.sum(-incorrect_counts[y == k][:, np.newaxis] * X[y == k], axis=0)
+	 #  dW[:, k] = wj + wy
+  # dW /= num_train # average out weights
+
+  # fully vectorized: rougjly 10x faster
+  # rows map to samples, colums map to class
+  # a value v in X_mask[i,j] adds row sample i to class j with multiple of v (1 except where j==y_j)
+  X_mask = np.zeros(margins.shape)
+  X_mask[margins > 0] = 1
+  # for each sample, find total number of classes where margin > 0
+  incorrectsamples_count = np.sum(X_mask, axis=1)
+  # adapt weight where j == y_j
+  X_mask[np.arange(num_train), y] -= incorrectsamples_count
+
+  dW = X.T.dot(X_mask) / num_train
+  # regularize the weights
+  dW += reg*W
+
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
